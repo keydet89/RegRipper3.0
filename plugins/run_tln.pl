3c2be2c1c36b97383f6d1dd68d4d3dcff8b3075c
Zer0-Tolerance <Zer0-Tolerance@users.noreply.github.com>
2022-12-23 01:09:09 +0100

    Update run_tln.pl
    fixed indent

--- plugins/run_tln.pl
+++ plugins/run_tln.pl
@@ -22,7 +22,7 @@ my %config = (hive          => "Software, NTUSER\.DAT",
 sub getConfig{return %config}
 
 sub getShortDescr {
-	return "[Autostart] Get autostart key contents from Software & ntuser.dat hives";
+  return "[Autostart] Get autostart key contents from Software & ntuser.dat hives";
 }
 sub getDescr{}
 sub getRefs {}
@@ -32,95 +32,95 @@ sub getVersion {return $config{version};}
 my $VERSION = getVersion();
 
 sub pluginmain {
-	my $class = shift;
-	my $hive = shift;
-	::logMsg("Launching run_tln v.".$VERSION);
+  my $class = shift;
+  my $hive = shift;
+  ::logMsg("Launching run_tln v.".$VERSION);
 
-	my %guess = ();
-	my $hive_guess = "";
-	my %guess = ::guessHive($hive);
-	foreach my $g (keys %guess) {
-		$hive_guess = $g if ($guess{$g} == 1);
-	}
+  my %guess = ();
+  my $hive_guess = "";
+  my %guess = ::guessHive($hive);
+  foreach my $g (keys %guess) {
+    $hive_guess = $g if ($guess{$g} == 1);
+  }
 
-	my $reg = Parse::Win32Registry->new($hive);
-	my $root_key = $reg->get_root_key;
-	my @paths = ();
+  my $reg = Parse::Win32Registry->new($hive);
+  my $root_key = $reg->get_root_key;
+  my @paths = ();
 
-	if ($hive_guess eq "software") {
-		@paths = ("Microsoft\\Windows\\CurrentVersion\\Run",
-	             "Microsoft\\Windows\\CurrentVersion\\RunOnce",
-	             "Microsoft\\Windows\\CurrentVersion\\RunServices",
-	             "Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run",
-	             "Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
-	             "Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run",
-	             "Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run",
-	             "Microsoft\\Windows NT\\CurrentVersion\\Terminal Server\\Install\\Software\\Microsoft\\Windows\\CurrentVersion\\Run",
-	             "Microsoft\\Windows NT\\CurrentVersion\\Terminal Server\\Install\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
-	             "Microsoft\\Windows\\CurrentVersion\\StartupApproved\\Run",
-	             "Microsoft\\Windows\\CurrentVersion\\StartupApproved\\Run32",
-	             "Microsoft\\Windows\\CurrentVersion\\StartupApproved\\StartupFolder"
-	             );
-	}
-	elsif ($hive_guess eq "ntuser") {
-		@paths = ("Software\\Microsoft\\Windows\\CurrentVersion\\Run",
-	           "Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run",
-	           "Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
-	           "Software\\Microsoft\\Windows\\CurrentVersion\\RunServices",
-	           "Software\\Microsoft\\Windows\\CurrentVersion\\RunServicesOnce",
-	           "Software\\Microsoft\\Windows NT\\CurrentVersion\\Terminal Server\\Install\\".
-	           "Software\\Microsoft\\Windows\\CurrentVersion\\Run",
-	           "Software\\Microsoft\\Windows NT\\CurrentVersion\\Terminal Server\\Install\\".
-	           "Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
-	           "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run",
-	           "Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run",
-	           "Software\\Microsoft\\Windows\\CurrentVersion\\StartupApproved\\Run",
-	           "Software\\Microsoft\\Windows\\CurrentVersion\\StartupApproved\\Run32",
-	           "Software\\Microsoft\\Windows\\CurrentVersion\\StartupApproved\\StartupFolder"
-	           );
-	}
-	else {}
+  if ($hive_guess eq "software") {
+    @paths = ("Microsoft\\Windows\\CurrentVersion\\Run",
+               "Microsoft\\Windows\\CurrentVersion\\RunOnce",
+               "Microsoft\\Windows\\CurrentVersion\\RunServices",
+               "Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run",
+               "Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
+               "Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run",
+               "Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run",
+               "Microsoft\\Windows NT\\CurrentVersion\\Terminal Server\\Install\\Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+               "Microsoft\\Windows NT\\CurrentVersion\\Terminal Server\\Install\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
+               "Microsoft\\Windows\\CurrentVersion\\StartupApproved\\Run",
+               "Microsoft\\Windows\\CurrentVersion\\StartupApproved\\Run32",
+               "Microsoft\\Windows\\CurrentVersion\\StartupApproved\\StartupFolder"
+               );
+  }
+  elsif ($hive_guess eq "ntuser") {
+    @paths = ("Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+             "Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run",
+             "Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
+             "Software\\Microsoft\\Windows\\CurrentVersion\\RunServices",
+             "Software\\Microsoft\\Windows\\CurrentVersion\\RunServicesOnce",
+             "Software\\Microsoft\\Windows NT\\CurrentVersion\\Terminal Server\\Install\\".
+             "Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+             "Software\\Microsoft\\Windows NT\\CurrentVersion\\Terminal Server\\Install\\".
+             "Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
+             "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run",
+             "Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run",
+             "Software\\Microsoft\\Windows\\CurrentVersion\\StartupApproved\\Run",
+             "Software\\Microsoft\\Windows\\CurrentVersion\\StartupApproved\\Run32",
+             "Software\\Microsoft\\Windows\\CurrentVersion\\StartupApproved\\StartupFolder"
+             );
+  }
+  else {}
 
-	foreach my $key_path (@paths) {
-		my $key;
-		if ($key = $root_key->get_subkey($key_path)) {
+  foreach my $key_path (@paths) {
+    my $key;
+    if ($key = $root_key->get_subkey($key_path)) {
       my $lw=$key->get_timestamp();
 
-			my %vals = getKeyValues($key);
-			if (scalar(keys %vals) > 0) {
+      my %vals = getKeyValues($key);
+      if (scalar(keys %vals) > 0) {
         my @names=keys %vals;
         my @autoruns;
-				foreach my $v (@names) {
-					# ::rptMsg("  ".$v." - ".$vals{$v});
+        foreach my $v (@names) {
+          # ::rptMsg("  ".$v." - ".$vals{$v});
           push(@autoruns,"$v -> ".$vals{$v}.";");
-				}
+        }
         ::rptMsg($lw."|REG|||Autoruns - Last Write key $key_path modified entries: ".scalar(keys %vals)." content: @autoruns");
-			}
-			else {
-				# ::rptMsg($key_path." has no values.");
-			}
+      }
+      else {
+        # ::rptMsg($key_path." has no values.");
+      }
 
-			my @sk = $key->get_list_of_subkeys();
-			if (scalar(@sk) > 0) {
-				foreach my $s (@sk) {
+      my @sk = $key->get_list_of_subkeys();
+      if (scalar(@sk) > 0) {
+        foreach my $s (@sk) {
           my $lw=$s->get_timestamp();
           my $subkey=$key_path."\\".$s->get_name();
-					my %vals = getKeyValues($s);
+          my %vals = getKeyValues($s);
           my @autoruns;
-					foreach my $v (keys %vals) {
+          foreach my $v (keys %vals) {
             push(@autoruns,"$v -> ".$vals{$v});
-					}
+          }
           ::rptMsg($lw."|REG|||Autoruns - Last Write key $subkey modified entries: ".scalar(keys %vals)." content: @autoruns");
-				}
-			}
-			else {
-				# ::rptMsg($key_path." has no subkeys.");
-			}
-		}
-		else {
-			# ::rptMsg($key_path." not found.");
-		}
-	}
+        }
+      }
+      else {
+        # ::rptMsg($key_path." has no subkeys.");
+      }
+    }
+    else {
+      # ::rptMsg($key_path." not found.");
+    }
+  }
 }
 
 
@@ -129,20 +129,20 @@ sub pluginmain {
 #
 #------------------------------------------------------------------------------
 sub getKeyValues {
-	my $key = shift;
-	my %vals;
+  my $key = shift;
+  my %vals;
 
-	my @vk = $key->get_list_of_values();
-	if (scalar(@vk) > 0) {
-		foreach my $v (@vk) {
-			next if ($v->get_name() eq "" && $v->get_data() eq "");
-			$vals{$v->get_name()} = $v->get_data();
-		}
-	}
-	else {
+  my @vk = $key->get_list_of_values();
+  if (scalar(@vk) > 0) {
+    foreach my $v (@vk) {
+      next if ($v->get_name() eq "" && $v->get_data() eq "");
+      $vals{$v->get_name()} = $v->get_data();
+    }
+  }
+  else {
 
-	}
-	return %vals;
+  }
+  return %vals;
 }
 
 1;
